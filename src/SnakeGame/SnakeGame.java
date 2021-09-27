c6b27894d036b9549068adfa5e1ee74cf40d4006
NothingToUnderstand <91472828+NothingToUnderstand@users.noreply.github.com>
2021-09-27 16:09:35 +0300

    Initial commit

---
+++ src/SnakeGame/SnakeGame.java
@@ -0,0 +1,10 @@
+package SnakeGame;
+
+import GameFrame.GameFrame;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+        new GameFrame();                     //GameFrame frame=new GameFrame;
+
+    }
+}
